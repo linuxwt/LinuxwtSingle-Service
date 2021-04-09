216e77a355776d025f41cff57b83ffa0341cc8d1
linuxwt <tengwanginit@gmail.com>
2021-04-09 17:29:11 +0800

    添加lnmp部署文件

---
+++ image/mysql/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mysqld --initialize-insecure --user=mysql > /dev/null &&
+mysqld  &
+sleep 5
+mysql < /tmp/config.sql
+sleep 5
+ps -wef | grep mysql | grep -v grep | awk '{print $2}' | xargs kill -9
+mysqld
